2a6de85a56d6c3b927dc392d888903e73ecd27bb
Unknown <ruben.pferrer@hotmail.de>
2017-05-25 15:16:16 +0200

    ADD new nav ADD tweening UPDATE nav controlling

--- js/application.js
+++ js/application.js
@@ -9,6 +9,35 @@ function init()
         paginationClickable: true,
         direction: 'vertical'
     };
+    var navContainer = document.querySelector('#navigation-container');
+    var navController = navContainer.querySelector('.burger-button');
+    var navListWrapper = navContainer.querySelector('.nav-list-wrapper');
+    var navItems = navContainer.querySelectorAll('.nav-list-item');
+
+    TweenMax.staggerFrom(navItems, 2,
+    {
+        x: 0,
+        force3D:true
+    }, 0.2);
+
+    navController.addEventListener('click', function(e)
+    {
+        var btn = e.currentTarget.parentNode;
+        var isActive = btn.classList.contains('active');
+        var isWrapperOpen = navListWrapper.classList.contains('open');
+
+
+        isActive ? btn.classList.remove('active') : btn.classList.add('active');
+        isWrapperOpen ? navListWrapper.classList.remove('open') : navListWrapper.classList.add('open');
+
+        setTimeout(function()
+        {
+            TweenMax.staggerTo(navItems, 0.5,
+            {
+                x: -600
+            }, 0.1);
+        }, 350);
+    });
 
     pageSwiper = new Swiper(swiperContainer, options);
 }
